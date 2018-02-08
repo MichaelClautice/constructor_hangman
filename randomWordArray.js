6384d5d70e1ea38fd26a45063498527c46cba84a
Michael Clautice <michaelclauticeusa@gmail.com>
2018-02-08 13:36:15 -0500

    Add files via upload

--- randomWordArray.js
+++ randomWordArray.js
@@ -1,5 +1,6 @@
+// game-player must guess one of these 6 words - letter by letter
 // a random word is selected and exported
-// here is an array of random words for the hangman game
+// here is an array of 6 random words for the hangman game
 var wordsArray = ['lizard', 'biscuit', 'radio', 'airplane', 'table', 'bike'];
 // select a random word from that array & assign it to a var called randomWord
 var randomWord = Math.floor(Math.random() * wordsArray.length);
--- wordConstructor.js
+++ wordConstructor.js
@@ -1,48 +1,94 @@
-// the require method is used to get access to the external letter constructor & assign it as a value to the variable letterConstruct
-
-var letterConstruct = require('./letterConstructor');
-
-// •••••••••HERE IS THE WORD CONSTRUCTOR•••••••••••
-
-// it will check the user's letter guesses against the letters in the random word from the wordsArray array in the file called randomWordArray.js
-
-// this word constructor will create an instantiation called word
-
-function wordConstructor(value) {
-// the value property will hold the value (of what? - the instantiation of this word constructor?)
-    this.value = value;
-// an array to hold letters
+// require() is used to access the external letter constructor & assign it as a value to the var letterConstruct √
+// ---
+// ??? is this right?
+// answer here:___________
+var letterConstructor = require('./letterConstructor');
+// ---
+// •••••••••HERE IS THE WORD CONSTRUCTOR••••••••••• √
+// ---
+// it will check the user's letter guesses against the letters in the random word from the wordsArray array in the file called randomWordArray.js √
+// ---
+// this word constructor can create an instantiation of a word made up from the game-player's sngl-letter-guesses √
+// ---
+// ??? what should wordConstructor's parameter be?
+// ??? won't it be using an instantiation of letter constructor?
+// answer here:___________
+function wordConstructor(word) {
+// ---
+// ??? the word property will hold the value of what? - an instantiation of wordConstructor()? √
+// answer here:___________
+    this.word = word;
+// ---
+// empty array which will hold the game-player's guessed letters from letterConstructor √
     this.lettersArray = [];
-// guessesMadeString is an empty string (for now)
-// guessesMadeString will hold the user's guessed letters
+// ---
+// guessesMadeString is an empty string (for now) and will hold the user's guessed letters
     this.guessesMadeString = '';
-// iterate thru 'value' & push ea letter into lettersArray
-    for(var i = 0; i < this.value.length; i++) {
-// this line shows where we take the value of the instantiation 'letterConstruct' from the letterConstructor.js file annnnd...
+// ---
+// ---
+// ---
+// ---
+// SUMMARY OF SECTION 01/04:
+// ---
+// iterate thru wordValue & push those letters into lettersArray
+// ---
+// •••••••••••••••••••START SECTION 01/04
+// ---
+// iterate thru 'word' & push ea instantiated letter into lettersArray
+// ---
+    for(var i = 0; i < this.word.length; i++) {
+// ---
+// next line shows where we take the value of an instantiation from 'letterConstructor' annnnd...
 // push it into the letters array
-        this.lettersArray.push(new letterConstruct.letterConstructor(this.value[i]));
+// ---
+// next line - REMEMBER! in this line, think from the inside to the outside!
+// ---
+// 1 - we take an individual array item from wordValue[i]
+// 2 - use that as an arg for letterConstructor()
+// 3 - keyword "new" tells JS that we want to create an instantiation from the letterConstructor
+// 4 - JS's push() function will "push" that instantiation into lettersArray[]
+        this.lettersArray.push(new letterConstructor(this.word[i]));
     }
 };
-
+// ••••••••••••••••••• END SECTION 01/04
+// ---
+// ---
+// ---
+// ---
 // instead of binding a method called isCompleteMethod inside the constructor we use the keyword prototype to define it outside the constructor
+// ---
+// ---
+// ---
+// ---
+// ••••••••••••••••••• START SECTION 02/04
 
+// ITERATE THRU lettersArray ONE LETTER AT A TIME AND...••••••••••••
+// if any of those array items are NOT keyboard values...
+// the boolean value of showLetter will be false
+// ---
 // isCompleteMethod is assigned a value of an anonymous function
-
+// ---
 // •••••••••HERE IS THE isCompleteMethod PROTOTYPE METHOD•••••••••••
 wordConstructor.prototype.isCompleteMethod = function(){
 // this function will iterate thru the lettersArray
     for(var i = 0; i < this.lettersArray.length; i++){
 // if any of the array items are NOT keyboard values...
-// the boolean value of show will be false
-        if(!this.lettersArray[i].show) return false;
+// the boolean value of showLetter will be false
+        if(!this.lettersArray[i].showLetter) return false;
     }
 // otherwise the boolean value of show will be true
-// apparently 'show' will be used elsewhere, right?????<<<<<<
+// apparently 'showLetter' will be used elsewhere, right?????<<<<<<
     return true;
 }
-
+// ••••••••••••••••••• END SECTION 02/04
+// ---
+// ---
+// ---
+// ---
+// ••••••••••••••••••• START SECTION 03/04
+// ---
 // •••••••••HERE IS THE findLetterMethod PROTOTYPE METHOD•••••••••••
-
+// ---
 // takes 'letterConstruct' as an arg from letterConstructor.js
 wordConstructor.prototype.findLetterMethod = function(letterConstruct){
 // converts it to lower case if needed & assigns it to a var
@@ -62,23 +108,34 @@ wordConstructor.prototype.findLetterMethod = function(letterConstruct){
 // if the lower case value of an item in the lettersArray array is equal to lowerLetter...
         if(this.lettersArray[i].value.toLowerCase() === lowerLetter){
 // than the boolean value of show is assigned true
-        this.lettersArray[i].show = true;
+        this.lettersArray[i].showLetter = true;
         }
     }
 };
-
+// ••••••••••••••••••• END SECTION 03/04
+// ---
+// ---
+// ---
+// ---
+// ••••••••••••••••••• START SECTION 04/04
+// ---
 // •••••••••HERE IS THE toStringMethod PROTOTYPE METHOD•••••••••••
 wordConstructor.prototype.toStringMethod = function(){
 // output is an empty string (for now)
   var output = '';
 // loop-iterate thru lettersArray &
   for(var i=0; i<this.lettersArray.length; i++){
-// hey - printLetterMethod() is defined in an external js file - is this okay?
-    output += this.lettersArray[i].printLetterMethod();
+// saveLetterMethod() is defined in an external js file
+    output += this.lettersArray[i].saveLetterMethod();
   }
   return output;
-}
-
+// ---
+// ••••••••••••••••••• END SECTION 04/04
+// ---
+// ---
+// ---
+// ---
 // exports is used to make this constructor instantiation available in other js files
-
+// ??? is this right?
+// answer here:___________
 exports.wordConstruct = wordConstructor;
